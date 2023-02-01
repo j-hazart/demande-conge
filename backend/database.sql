2413116151cb3889ec4e793382fd6be7a0a61d5b
jessy hazart <jessy.hazart@gmail.com>
2023-02-01 11:41:07 +0100

    added conge to database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -11,3 +11,16 @@ INSERT INTO user (prenom, nom, email, mdp, role) VALUES
 ("Le", "Boss", "boss@mail.fr", "boss", "patron"),
 ("John", "Doe", "johndoe@mail.fr", "doe", "employe");
 
+CREATE TABLE conge (
+  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  dateDebut date NOT NULL,
+  dateFin date NOT NULL,
+  statut varchar(10) NOT NULL,
+  user_id int(11) unsigned NOT NULL,
+  constraint conge_user foreign key (user_id) references user(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO conge (dateDebut, dateFin, statut, user_id) VALUES
+("2023-03-15", "2023-04-15", "attente", 2),
+("2023-02-15", "2023-02-25", "valide", 2),
+("2023-02-10", "2023-03-11", "refus", 2);
